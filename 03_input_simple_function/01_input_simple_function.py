d4c598bc0e0e00926c4a9fce79259e14ac76c00a
ohadz1906 <ohadzouzout1@gmail.com>
2023-07-24 20:58:34 +0300

    adding 03_input_simple_function.py

---
+++ 03_input_simple_function/01_input_simple_function.py
@@ -0,0 +1,11 @@
+# Scanner input = new Scanner(System.in)
+# input.nexLine()
+
+name = input("Please enter your name: ")
+age= input("Please enter your age: ")
+print(name, type(name), age, type(age))
+
+print(name + age)
+
+
+
---
+++ 03_input_simple_function/02_input_numbers.py
@@ -0,0 +1,4 @@
+number_1 = int(input("Please enter 1st number: "))
+number_2 = int(input("Please enter 2nd number: "))
+print(type(number_2))
+print(f"{number_1} + {number_2} = {number_1 + number_2}")

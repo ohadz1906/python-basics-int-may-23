0a9e4097be183b20b28ca79d1a22ef34219af2d0
ohadz1906 <ohadzouzout1@gmail.com>
2023-07-24 18:28:37 +0300

    adding 04_arithmetic_operators.py

---
+++ lesson_01_variables/04_arithmetic_operators.py
@@ -0,0 +1,44 @@
+# number_1 = 5
+# number_2 = 3
+#
+# print(f"{number_1}+{number_2} = {(number_1 + number_2)}")
+#
+# print(f"{number_1}-{number_2} = {(number_1 - number_2)}")
+#
+# print(f"{number_1}*{number_2} = {(number_1 * number_2)}")
+#
+# print(type(number_1 * number_2))
+#
+# number_3 = 2.0
+# print(f"{number_1}*{number_3} = {(number_1 * number_3)}")
+# print(type(number_1 * number_3))
+#
+# print(f"{number_1}/{number_2} = {(number_1 / number_2)}")
+# number_1 = 2
+# number_2 = 5
+# print(f"{number_1}**{number_2} = {(number_1 ** number_2)}")
+
+
+#--------------------------------------
+
+number_1 = 15
+number_2 = 5
+
+print(f"{number_1}//{number_2} = {(number_1 // number_2)}")
+
+number_1 = 16
+number_2 = 5
+print(f"{number_1}//{number_2} = {(number_1 // number_2)}")
+print(f"{number_1}%{number_2} = {(number_1 % number_2)}")
+
+number_1 = 5
+number_2 = 15
+print(f"{number_1}//{number_2} = {(number_1 // number_2)}")
+print(f"{number_1}%{number_2} = {(number_1 % number_2)}")
+print(25 % 2)
+print(24 % 2)
+
+
+
+
+

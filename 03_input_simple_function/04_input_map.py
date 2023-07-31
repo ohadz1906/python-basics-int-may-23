1b24ffd58726580174da9aab89e7db7ebfe4a704
ohadz1906 <ohadzouzout1@gmail.com>
2023-07-31 18:45:56 +0300

    adding file print

--- 03_input_simple_function/04_input_map.py
+++
@@ -1,2 +0,0 @@
-score_1, score_2, score_3 = map(int, input("enter3 scores: ").split())
-print(score_1 + score_2 + score_3)
---
+++ 03_input_simple_print_function/04_input_map.py
@@ -0,0 +1,2 @@
+score_1, score_2, score_3 = map(int, input("enter 3 scores: ").split())
+print(score_1 + score_2 + score_3)

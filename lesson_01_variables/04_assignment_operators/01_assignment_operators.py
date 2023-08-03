81c24daf86ae292fcba001a0fce12f1fd326e83e
ohadz1906 <ohadzouzout1@gmail.com>
2023-08-03 18:13:59 +0300

    adding assignment

---
+++ lesson_01_variables/04_assignment_operators/01_assignment_operators.py
@@ -0,0 +1,36 @@
+number = 10
+
+print(f"number = {number}")
+
+number = number + 1
+
+print(f" \nnumber = number + 1 \n---> number = {number}")
+
+number += 1
+print(f"\nnumber += 1 \n---> number = {number}")
+################ -= #################
+number -= 1
+print(f"\nnumber += 1 \n---> number = {number}")
+
+
+################ *= #################
+number *= 5
+print(f"\nnumber *= 5 \n---> number = {number}")
+
+
+################ /= #################
+number /= 2
+print(f"\nnumber /= 2 \n---> number = {number}")
+
+################ %= #################
+number %= 10
+print(f"\nnumber %= 10 \n---> number = {number}")
+
+################ //= #################
+number //= 1
+print(f"\nnumber //= 1 \n---> number = {number}")
+
+################ **= #################
+number **= 1
+print(f"\nnumber **= 1 \n---> number = {number}")
+

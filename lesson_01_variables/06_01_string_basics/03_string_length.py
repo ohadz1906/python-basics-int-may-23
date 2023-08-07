d27559d180a7dde151c060f80d6772e1900c1986
ohadz1906 <ohadzouzout1@gmail.com>
2023-08-07 19:05:46 +0300

    adding files

---
+++ lesson_01_variables/06_01_string_basics/03_string_length.py
@@ -0,0 +1,23 @@
+name = "Ohad"
+
+print(len(name))
+
+print(len(" "))
+print(len(""))
+
+message = input ("Enter your message")
+print("Your message is: ", message)
+print(f"You entered {len(message)} symbols")
+
+
+
+
+
+
+
+
+
+
+
+
+

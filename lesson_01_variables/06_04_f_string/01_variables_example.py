729c4eafb1316562645801d84341881e6a6be436
ohadz1906 <ohadzouzout1@gmail.com>
2023-08-31 17:44:03 +0300

    adding 03_input_simple_function.py

---
+++ lesson_01_variables/06_04_f_string/01_variables_example.py
@@ -0,0 +1,4 @@
+platform_name = input("wich platform do you use? ")
+action = input("What do you do? (study / teach")
+
+print("I" + action + "on" + platform_name )
---
+++ part_02lists/01_basics/03_list_duplications.py
@@ -0,0 +1,6 @@
+names = ["Alex", "Tal", "Andrei", "Tal", "Noam", "Shahar", "Noam"]
+print(names)
+
+names_set = set(names)
+print(type(names_set))
+print(names_set)
